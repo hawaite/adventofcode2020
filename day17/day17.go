ce16def4d91e0cd033d4ea7de449291a5caeb835
Helier Waite <helierawaite@gmail.com>
2024-05-07 22:58:29 +0100

    day 17, part 2 - lazy bruteforcing, but learning a lot about goroutines

--- day17/day17.go
+++ day17/day17.go
@@ -1,22 +1,35 @@
 package day17
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type dimensionKey struct {
 	x int
 	y int
 	z int
+	w int
 }
 
-func countAdjacent(current_location dimensionKey, dimension map[dimensionKey]bool) int {
+func countAdjacent(current_location dimensionKey, dimension map[dimensionKey]bool, checkFourthDimension bool) int {
 	total := 0
 	// for each of the 24 possible adjacent locations, see if it exists in the given dimension
 	for x := current_location.x - 1; x <= current_location.x+1; x++ {
 		for y := current_location.y - 1; y <= current_location.y+1; y++ {
 			for z := current_location.z - 1; z <= current_location.z+1; z++ {
-				exists, is_active := dimension[dimensionKey{x, y, z}]
-				if exists && is_active {
-					total += 1
+				if checkFourthDimension {
+					for w := current_location.w - 1; w <= current_location.w+1; w++ {
+						exists, is_active := dimension[dimensionKey{x, y, z, w}]
+						if exists && is_active {
+							total += 1
+						}
+					}
+				} else {
+					exists, is_active := dimension[dimensionKey{x, y, z, 0}]
+					if exists && is_active {
+						total += 1
+					}
 				}
 			}
 		}
@@ -30,13 +43,19 @@ func countAdjacent(current_location dimensionKey, dimension map[dimensionKey]boo
 	return total
 }
 
-func buildBlankDimension() map[dimensionKey]bool {
+func buildBlankDimension(hasFourDimensions bool) map[dimensionKey]bool {
 	dimension := map[dimensionKey]bool{}
-
-	for x := -20; x <= 20; x++ {
-		for y := -20; y <= 20; y++ {
-			for z := -20; z <= 20; z++ {
-				dimension[dimensionKey{x, y, z}] = false
+	i := 15
+	for x := -i; x <= i; x++ {
+		for y := -i; y <= i; y++ {
+			for z := -i; z <= i; z++ {
+				if hasFourDimensions {
+					for w := -i; w <= i; w++ {
+						dimension[dimensionKey{x, y, z, w}] = false
+					}
+				} else {
+					dimension[dimensionKey{x, y, z, 0}] = false
+				}
 			}
 		}
 	}
@@ -44,39 +63,59 @@ func buildBlankDimension() map[dimensionKey]bool {
 	return dimension
 }
 
-func Run(lines []string) (part1_res string, part2_res string) {
+func calculateAndSetLocationOnDimension(
+	location dimensionKey,
+	current_dimension map[dimensionKey]bool,
+	target_dimension map[dimensionKey]bool,
+	checkFourthDimension bool,
+	waitGroup *sync.WaitGroup,
+	mutex *sync.Mutex) {
 
-	dimension := buildBlankDimension()
+	adjacent_count := countAdjacent(location, current_dimension, checkFourthDimension)
+	is_active := current_dimension[location]
+
+	mutex.Lock()
+	if is_active && !(adjacent_count == 2 || adjacent_count == 3) {
+		// is active, make it inactive
+		target_dimension[location] = false
+	} else if !is_active && adjacent_count == 3 {
+		// is inactive, make it active
+		target_dimension[location] = true
+	} else {
+		//all other options result in current location being unchanged
+		target_dimension[location] = current_dimension[location]
+	}
+	mutex.Unlock()
+
+	waitGroup.Done()
+}
+
+func simulate(lines []string, useFourthDimension bool) int {
+	dimension := buildBlankDimension(useFourthDimension)
 
 	// read initial state
 	for iy, line := range lines {
 		for ix, char := range line {
 			if char == '#' {
-				dimension[dimensionKey{ix, iy, 0}] = true
+				dimension[dimensionKey{ix, iy, 0, 0}] = true
 			}
 		}
 	}
 
 	dimension_iteration_count := 6
+	var waitGroup sync.WaitGroup
+	var mutex sync.Mutex
 
-	for i := range dimension_iteration_count {
+	for range dimension_iteration_count {
 		// create new blank dimension
-		next_dimension := buildBlankDimension()
+		next_dimension := buildBlankDimension(useFourthDimension)
 
-		// for every cube in the dimension, apply rules
-		for location, is_active := range dimension {
-			adjacent_count := countAdjacent(location, dimension)
-			if is_active && !(adjacent_count == 2 || adjacent_count == 3) {
-				// is active, make it inactive
-				next_dimension[location] = false
-			} else if !is_active && adjacent_count == 3 {
-				// is inactive, make it active
-				next_dimension[location] = true
-			} else {
-				//all other options result in current location being unchanged
-				next_dimension[location] = dimension[location]
-			}
+		for location := range dimension {
+			waitGroup.Add(1)
+			go calculateAndSetLocationOnDimension(location, dimension, next_dimension, useFourthDimension, &waitGroup, &mutex)
 		}
+
+		waitGroup.Wait()
 
 		total_active_in_iteration := 0
 		for _, is_active := range next_dimension {
@@ -85,7 +124,6 @@ func Run(lines []string) (part1_res string, part2_res string) {
 			}
 		}
 
-		fmt.Printf("Total active in generation %d: %d\n", i, total_active_in_iteration)
 		dimension = next_dimension
 	}
 
@@ -96,8 +134,11 @@ func Run(lines []string) (part1_res string, part2_res string) {
 		}
 	}
 
-	part1_res = fmt.Sprintf("%d", total_active_in_iteration)
-	// fmt.Println(dimension)
-	// 301 too low
+	return total_active_in_iteration
+}
+
+func Run(lines []string) (part1_res string, part2_res string) {
+	part1_res = fmt.Sprintf("%d", simulate(lines, false))
+	part2_res = fmt.Sprintf("%d", simulate(lines, true))
 	return part1_res, part2_res
 }
